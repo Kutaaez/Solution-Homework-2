44e8bdecee57be90a1d6bc16c5aa9108414a447d
Kutaez <m_jasik_2005@mail.ru>
2025-02-22 12:04:50 +0500

    finalize project

--- src/MUDController.java
+++ src/MUDController.java
@@ -1,6 +1,6 @@
 
-import java.util.Scanner;
-import com.example.mud.player.Player;
+import java.util.*;
+
 
 /**
  * MUDController (Skeleton):
@@ -12,15 +12,34 @@ public class MUDController {
 
     private Player player;
     private boolean running;
+    private final Map<String, String> rooms = new HashMap<>();
+    private final Map<String, List<String>> roomItems = new HashMap<>();
+    private final Map<String, String> roomNpcs = new HashMap<>();
+    private String currentRoom;
+    private String previousRoom;
 
     /**
      * Constructs the controller with a reference to the current player.
      */
     public MUDController(Player player) {
 
-        // Initialize fields here (if needed)
-    }
+        this.player = player;
+        this.running = true;
+        initializeGame();    }
 
+    public void initializeGame(){
+        rooms.put("legend_hall", "A grand hall filled with ancient books and artifacts, glowing softly in the dim light.");
+        rooms.put("dark_sketches", "A room adorned with eerie paintings of mythical creatures and ominous scenes.");
+        rooms.put("storm_tower", "A towering chamber where the winds never cease, filled with enchanted relics and ancient weapons.");
+
+        roomItems.put("legend_hall", new ArrayList<>(Arrays.asList("healing herb")));
+        roomItems.put("dark_sketches", new ArrayList<>(Arrays.asList("sword of destiny")));
+        roomItems.put("storm_tower", new ArrayList<>(Arrays.asList("gold coin")));
+
+        roomNpcs.put("legend_hall", "Guardian of the Grove");
+        roomNpcs.put("dark_sketches", "Wandering Sorcerer");
+        roomNpcs.put("storm_tower", "Ancient Priestess");
+    }
     /**
      * Main loop method that repeatedly reads input from the user
      * and dispatches commands until the game ends.
@@ -32,7 +51,11 @@ public class MUDController {
         while (running) {
             System.out.println("> ");
             String input = scanner.nextLine().trim().toLowerCase();
-            running = handleInput(input);
+            if (input.equals("quit") || input.equals("exit")) {
+                running = false;
+            } else {
+                handleInput(input);
+            }
         }
         System.out.println("Game stopped");
         scanner.close();
@@ -47,16 +70,75 @@ public class MUDController {
      * Handle a single command input (e.g. 'look', 'move forward', 'pick up sword').
      */
     public void handleInput(String input) {
+        String[] parts = input.split(" ", 2);
+        String command = parts[0];
+        String argument = parts.length > 1 ? parts[1] : "";
+
+        switch (command) {
+            case "look":
+                lookAround(currentRoom);
+                break;
+            case "attack":
+                attackNPC(currentRoom);
+                break;
+
+            case "talk":
+                talkToNPC(currentRoom);
+                break;
+            case "move":
+                move(argument);
+                break;
+            case "pick":
+                if (argument.startsWith("up ")) {
+                    pickUp(argument.substring(3));
+                } else {
+                    System.out.println("Invalid pick up command. Use 'pick up <item>'.");
+                }
+                break;
+            case "inventory":
+                checkInventory();
+                break;
+            case "help":
+                showHelp();
+                break;
+            case "quit":
+            case "exit":
+                System.out.println("Exiting the game...");
+                break;
+            default:
+                System.out.println("Unknown command. Type 'help' for a list of commands.");
+        }
         // TODO:
         // 1) Parse the input into a command and optionally an argument
         // 2) Use a switch/case (or if/else) to call the correct method below
         //    based on the command word
     }
-
+public void attackNPC(String currentRoom){
+    System.out.println("You are MONSTER!");
+    roomNpcs.remove(currentRoom);
+}
+public void talkToNPC(String currentRoom){
+    System.out.println("You talked with " + roomNpcs.get(currentRoom));
+}
     /**
      * Look around the current room: describe it and show items/NPCs.
      */
-    private void lookAround() {
+    private void lookAround(String currentRoom) {
+        System.out.println("Player are in " + currentRoom + ". \n ");
+        if(roomItems.get(currentRoom) != null){
+            System.out.println("You see  some items " + roomItems.get(currentRoom) + ".");
+        }
+        else {
+            System.out.println("There’s nothing interesting here.");
+
+        }
+        if(roomItems.get(currentRoom) != null){
+            System.out.println("You see someone" + roomNpcs.get(currentRoom)+ ".");
+        }
+        else {
+            System.out.println("No one else is here.");
+
+        }
         // TODO: Print information about the player's current room
     }
 
@@ -64,6 +146,43 @@ public class MUDController {
      * Move the player in a given direction (forward, back, left, right).
      */
     private void move(String direction) {
+
+        String[] availableRooms = rooms.keySet().toArray(new String[0]);
+        Random random = new Random();
+        switch (direction) {
+            case "forward":
+                System.out.println("You move forward into the next room.");
+                if (currentRoom != null) {
+                    previousRoom = currentRoom;
+                }
+                currentRoom = availableRooms[random.nextInt(availableRooms.length)];
+                break;
+            case "back":
+                if (previousRoom != null) {
+                    System.out.println("You move back to the previous room.");
+                    currentRoom = previousRoom;
+                    previousRoom = null;
+                } else {
+                    System.out.println("There's no way back.");
+                }
+                break;
+            case "left":
+                System.out.println("You turn left and walk into a new space.");
+                previousRoom = currentRoom;
+                currentRoom = availableRooms[random.nextInt(availableRooms.length)];
+                break;
+            case "right":
+                System.out.println("You turn right and find a new area.");
+                previousRoom = currentRoom;
+                currentRoom = availableRooms[random.nextInt(availableRooms.length)];
+                break;
+            default:
+                System.out.println("Invalid direction. Try 'forward', 'back', 'left', or 'right'.");
+                return;
+
+        }
+        System.out.println("You are now in "+ currentRoom);
+
         // TODO: Attempt to move to the next room in the given direction
         //       If there's no room in that direction, print an error message
         //       If successfully moved, describe the new room
@@ -73,6 +192,15 @@ public class MUDController {
      * Pick up an item (e.g. "pick up sword").
      */
     private void pickUp(String arg) {
+        List<String> itemsInRoom = roomItems.get(currentRoom);
+
+        if (itemsInRoom != null && itemsInRoom.contains(arg.toLowerCase())) {
+            player.addItemToInventory(arg);
+            itemsInRoom.remove(arg);
+            System.out.println("You picked up the " + arg + ".");
+        } else {
+            System.out.println("There's no such item here.");
+        }
         // TODO:
         // 1) Parse out the item name if 'arg' starts with "up "
         // 2) Check if that item exists in the current room
@@ -83,6 +211,7 @@ public class MUDController {
      * Check the player's inventory.
      */
     private void checkInventory() {
+        System.out.println("Player inventory : " + player.showInventory());
         // TODO: List the items in the player's inventory
         //       If no items, indicate that the inventory is empty
     }
@@ -91,6 +220,16 @@ public class MUDController {
      * Show help commands
      */
     private void showHelp() {
+        System.out.println("Available commands:");
+        System.out.println("look - Describe the current room.");
+        System.out.println("talk <NPC> - Talk to the NPC in the room");
+        System.out.println("move <forward|back|left|right> - Move in a direction.");
+        System.out.println("pick up <item> - Pick up an item.");
+        System.out.println("inventory - List the items you are carrying.");
+        System.out.println("help - Show this help message.");
+        System.out.println("quit / exit - End the game.");
+        System.out.println("attack - you can attack npc, but I strongly advise against doing so at the moment... ");
+
         // TODO: Print a list of available commands and brief instructions
     }
 
--- src/Player.java
+++ src/Player.java
@@ -4,39 +4,32 @@ import java.util.List;
 public class Player {
     private String name;
     private List<String> inventory;
-    private String currentRoom;
 
     public Player() {
+        this.inventory = new ArrayList<>();
+
     }
 
     public Player(String name) {
         this.name = name;
         this.inventory = new ArrayList<>();
-        this.currentRoom = "Starting Room"; // Initial room
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCurrentRoom() {
-        return currentRoom;
-    }
-
-    public void setCurrentRoom(String newRoom) {
-        this.currentRoom = newRoom;
-    }
 
     public void addItemToInventory(String item) {
         inventory.add(item);
         System.out.println(item + " added to your inventory.");
     }
 
-    public void showInventory() {
+    public String showInventory() {
         if (inventory.isEmpty()) {
-            System.out.println("Your inventory is empty.");
+            return " is empty.";
         } else {
-            System.out.println("Your inventory: " + inventory);
+            return " " + inventory;
         }
     }
 }
